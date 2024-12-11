870210d61815fd9299b78b3e8ddd31a70733d5c4
sbmsr <sebastian.messier@gmail.com>
2024-12-11 18:28:32 -0500

    2

--- 2024/deno/2/index.ts
+++ 2024/deno/2/index.ts
@@ -11,28 +11,56 @@ function loadInput() {
   return reports;
 }
 
-function part1(reports: number[][]) {
-  const isReportValid = (report: number[], increasing?: boolean): boolean => {
-    const [first, second, ...rest] = report;
-    if (first === undefined) return true;
-    if (second === undefined) return true;
-    if (increasing === undefined) {
-      increasing = first < second;
-    }
-    const isValid = increasing ? first + 3 >= second && first < second : first - 3 <= second && first > second;
-    return rest === undefined ? isValid : isValid && isReportValid([second, ...rest], increasing);
-  };
+const isReportValid = (report: number[], increasing?: boolean): boolean => {
+  const [first, second, ...rest] = report;
+  if (first === undefined) return true;
+  if (second === undefined) return true;
+  if (increasing === undefined) {
+    increasing = first < second;
+  }
+  const isValid = increasing ? first + 3 >= second && first < second : first - 3 <= second && first > second;
+  return rest === undefined ? isValid : isValid && isReportValid([second, ...rest], increasing);
+};
 
+function part1(reports: number[][]) {
   const results = reports.map((report) => isReportValid(report));
   return results.filter((res) => res === true).length;
 }
 
-// part 2
+// reports is number[][]
+// [
+//  [...],
+//  ...
+// ]
+
+function part2(reports: number[][]) {
+  const generateReportsWithOneNumberRemoved = (report: number[]): number[][] => {
+    return report.map((_, i) => report.filter((_, j) => i !== j));
+  };
+
+  // reportsPlusOmissions is numbers[][][]
+  // [
+  //  [[...], ...]
+  //  ...
+  // ]
+
+  const reportsPlusOmissions = reports.map((report) => [report].concat(generateReportsWithOneNumberRemoved(report)));
+
+  // turn it into boolean[]
+  // [
+  //  true/false,
+  //  ...
+  // ]
+
+  return reportsPlusOmissions
+    .flatMap((reports) => reports.map((r) => isReportValid(r)).reduce((a, b) => a || b))
+    .filter((v) => v).length;
+}
 
 function main() {
   const reports = loadInput();
   console.log("Part 1:", part1(reports));
-  console.log("Part 2:");
+  console.log("Part 2:", part2(reports));
 }
 
 main();
