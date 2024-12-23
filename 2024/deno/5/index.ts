df7e1fa0c08b3218dcb0f804a557eba1ab068286
sbmsr <sebastian.messier@gmail.com>
2024-12-23 01:06:23 -0500

    5

--- 2024/deno/5/index.ts
+++ 2024/deno/5/index.ts
@@ -1,5 +1,7 @@
+type Input = { pairs: number[][]; lists: number[][] };
+
 function parseInput() {
-  const data: { pairs: number[][]; lists: number[][] } = { pairs: [], lists: []};
+  const data: Input = { pairs: [], lists: [] };
   Deno.readTextFileSync("./input.txt")
     .split("\n")
     .forEach((line) => {
@@ -7,35 +9,42 @@ function parseInput() {
         const [left, right] = line.split("|").map(Number);
         data.pairs.push([left, right]);
       } else if (line.includes(",")) {
-        data.lists.push(line.split(",").map(x => Number(x)))
+        data.lists.push(line.split(",").map((x) => Number(x)));
       }
     });
 
-    return data;
+  return data;
 }
 
-function part1(data: { pairs: number[][]; lists: number[][] }) {
-  const precedenceMap: Record<number, Set<number>> = {};
+function buildDependencyMap(pairs: Input["pairs"]): Map<number, Set<number>> {
+  const dependencyMap: Map<number, Set<number>> = new Map();
 
-  // 1. Build the map
-  for (const [left, right] of data.pairs) {
-    precedenceMap[left] === undefined ? (precedenceMap[left] = new Set([right])) : precedenceMap[left].add(right);
+  for (const [left, right] of pairs) {
+    dependencyMap.get(left) === undefined
+      ? dependencyMap.set(left, new Set([right]))
+      : dependencyMap.get(left)?.add(right);
   }
 
-  console.log(precedenceMap)
+  return dependencyMap;
+}
 
-  // 2. use map to determine valid lists
-  const validIdxs: number[] = [];
+function getListIdxs(lists: Input["lists"], dependencyMap: Map<number, Set<number>>, valid = true) {
+  // use map to determine valid lists
+  const idxs: number[] = [];
 
-  for (const [listIdx, list] of data.lists.entries()) {
-    console.log("working on list:", list)
+  for (const [listIdx, list] of lists.entries()) {
+    // console.debug("working on list:", list);
     const seenSet = new Set();
     let failed = false;
-    
+
     for (let idx = 0; idx < list.length; idx++) {
       const listItem = list[idx];
-      if (Array.from(precedenceMap[listItem] || []).some(x => seenSet.has(x))) {
-        console.log(`List invalid because ${listItem} must come before ${Array.from(precedenceMap[listItem].intersection(seenSet)).join(",")}`);
+      if (Array.from(dependencyMap.get(listItem) || []).some((x) => seenSet.has(x))) {
+        // console.debug(
+        //   `List invalid because ${listItem} must come before ${Array.from(
+        //     dependencyMap[listItem].intersection(seenSet)
+        //   ).join(",")}`
+        // );
         failed = true;
         break;
       } else {
@@ -43,21 +52,89 @@ function part1(data: { pairs: number[][]; lists: number[][] }) {
       }
     }
 
-    if (!failed) {
-      validIdxs.push(listIdx);
+    if (valid && !failed) {
+      idxs.push(listIdx);
+    } else if (!valid && failed) {
+      idxs.push(listIdx);
     }
   }
 
-  // 3. for each valid list, sum middle elements
-  return Array.from(data.lists.entries())
-    .filter(([idx]) => validIdxs.includes(idx))
+  return idxs;
+}
+
+function part1(inputData: Input) {
+  const dependencyMap = buildDependencyMap(inputData.pairs);
+
+  const validListIdxs = getListIdxs(inputData.lists, dependencyMap, true);
+
+  // for each valid list, sum middle elements
+  return Array.from(inputData.lists.entries())
+    .filter(([idx]) => validListIdxs.includes(idx))
     .map(([_, val]) => val[Math.floor(val.length / 2)])
     .reduce((x, y) => x + y, 0);
+}
+
+function part2({ pairs, lists }: Input) {
+  // find the invalid lists
+  const dependencyMap = buildDependencyMap(pairs);
+  const invalidListIdxs = getListIdxs(lists, dependencyMap, false);
+
+  // for each one, fix the list
+
+  const fixedLists = [];
+
+  for (const idx of invalidListIdxs) {
+    fixedLists.push(topologicalSort(lists[idx], dependencyMap));
+  }
+
+  // sum middle values
+  return fixedLists.map((l) => l[Math.floor(l.length / 2)]).reduce((l, r) => l + r);
+}
+
+function topologicalSort(list: number[], dependencyMap: Map<number, Set<number>>) {
+  // kahn's algorithm
+
+  const result: number[] = [];
+  const depCounts: Map<number, number> = new Map(); // {number}: {depCount}
+
+  for (const num of list) {
+    depCounts.set(num, 0);
+  }
+
+  // Build depCounts map for numbers in this list
+  for (const num of list) {
+    // count relevant dependencyMap entries
+    let predecesorCount = 0;
+    for (const [from, to] of dependencyMap.entries()) {
+      if (to.has(num) && list.includes(from)) {
+        predecesorCount++;
+      }
+    }
+    depCounts.set(num, predecesorCount);
+  }
+
+  while (result.length !== list.length) {
+    for (const [num, depCount] of depCounts) {
+      if (depCount === 0) {
+        result.push(num);
+        for (const dependency of dependencyMap.get(num)?.values() ?? []) {
+          const currentCount = depCounts.get(dependency);
+          if (currentCount !== undefined) {
+            depCounts.set(dependency, currentCount - 1);
+          }
+        }
+        depCounts.delete(num);
+      }
+    }
+  }
+
+  return result;
 }
 
 function main() {
   const input = parseInput();
   console.log("part1:", part1(input));
+  console.log("part2:", part2(input));
 }
 
 main();
